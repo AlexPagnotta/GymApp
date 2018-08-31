ff3899b5f109f67532be44be9fc99c3207aafb8d
Alex Pagnotta <alexpagnottacs@gmail.com>
2018-08-31 11:46:44 +0200

    Add exercise model

---
+++ app/src/main/java/com/example/alex/gymapp/model/Exercise.kt
@@ -0,0 +1,21 @@
+package com.example.alex.gymapp.model
+
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+
+open class Exercise(
+        @PrimaryKey var id: Long = 0,
+
+        var name: String = "",
+
+        var weight: Double = 0.0,
+
+        var restTime: Double = 0.0,
+
+        var series: Int = 1,
+
+        var repetitions : Int = 1,
+
+        var executionDay : String = ""
+
+) : RealmObject()
