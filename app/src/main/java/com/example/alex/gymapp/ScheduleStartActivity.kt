89dd6135e226a4602f8f3ee97f8a6285204d8cb1
Alex Pagnotta <alex.pagnotta@outlook.it>
2018-11-11 14:32:32 +0100

    Add schedule run fragment

---
+++ app/src/main/java/com/example/alex/gymapp/ScheduleStartActivity.kt
@@ -0,0 +1,12 @@
+package com.example.alex.gymapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ScheduleStartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule_start)
+    }
+}
