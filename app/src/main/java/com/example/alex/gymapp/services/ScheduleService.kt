0cf992fc39e166e24a98e6042a5af2230e958c6a
Alex Pagnotta <alex.pagnotta@outlook.it>
2018-11-18 15:41:46 +0100

    Initial work on service

---
+++ app/src/main/java/com/example/alex/gymapp/services/ScheduleService.kt
@@ -0,0 +1,12 @@
+package com.example.alex.gymapp.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class ScheduleService : Service() {
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
